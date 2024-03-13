4b5b12a360a9e4871e1860a1afe28edc027d429c
Jonathan Pribesh <jrpribs@gmail.com>
2024-03-12 20:04:14 -0400

    save first function scaffolding

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
@@ -17,3 +5,4 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+export { sendWelcomeEmail } from './send-welcome-email';
